82996ad1569e44965d097cb63d40eb7aa73a52c1
Halis Furkan Akgül <78754779+hfurkanakgul@users.noreply.github.com>
2022-05-28 01:04:49 +0300

    Add files via upload

---
+++ ChatApplication-main/ChatApplication-main/ChatApp/src/Message/Message.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Message;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author furkan
+ */
+public class Message implements java.io.Serializable{
+    public enum messageType {isim,mesaj,baglanClient, ChatGrupBaglanti,
+        roomPrivUpdate, ozelOdaGiris, roomPrivList, ozelOdaOlustur};    //ilgili alanlara göre mesaj tipleri belirlenir
+    public messageType tip;                                             //mesaj 
+    public String kendi;                                                //sahibi
+    public Object icerik;                                               //içerik
+    public ArrayList<String> userList;                                  //mesajın ulaşacağı kişilerin listesi
+    public String _room; //oda adı
+    public boolean PrivateRoom = false;                                 //chat grubu
+    public HashMap<String, ArrayList<String>> roomList;                 //oda listesi ve kullanıcılar
+    public String[] privRoomListe;                                      //özel oda
+    public Message(messageType _type){
+        tip = _type;
+    }
+}
